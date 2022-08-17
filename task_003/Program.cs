301d210ccd95efe8fc589ee1c54a5d1b276aa5a9
errasti <errasti007@yandex.ru>
2022-08-17 14:49:35 +0300

    Initial commit

---
+++ task_003/Program.cs
@@ -0,0 +1,23 @@
+﻿int [,] array = {
+    {1, 2, 3},
+    {4, 6, 1},
+    {2, 1, 6}
+};
+
+int[] sumNumbers = new int[10];
+
+for ( int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        sumNumbers[array[i,j]] ++;
+    }
+}
+
+for ( int i = 0; i < sumNumbers.Length; i++ )
+{
+    if (sumNumbers[i] != 0)
+    { 
+    Console.WriteLine("Элемент " + i + " встречается " + sumNumbers[i] + " раз");
+    }
+}
