d9af0d767c491087886045081d619685895cc06c
errasti <errasti007@yandex.ru>
2022-08-18 10:53:01 +0300

    Initial commit

---
+++ hw_01/Program.cs
@@ -0,0 +1,57 @@
+﻿int[,] CreateArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ModifyArray(int[,] array)
+{
+    int temp = 0;
+    for ( int i = 0; i < array.GetLength(0); i++ )
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    temp = array[i, k + 1];
+                    array[i,k + 1] = array[i,k];
+                    array[i,k] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+Console.WriteLine("Введите количество строк и столбцов через пробел: ");
+string[] input = Console.ReadLine().Split();
+int row = int.Parse(input[0]);
+int col = int.Parse(input[1]);
+int[,] myArray = CreateArray(row, col);
+ShowArray(myArray);
+ModifyArray(myArray);
+Console.WriteLine();
+ShowArray(myArray);
